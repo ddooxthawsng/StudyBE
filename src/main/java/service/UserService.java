32e7eeff43605988f64cf8a42f37479b7077aa40
thangdm12 <thangdm12@fpt.com.vn>
2023-03-15 15:25:00 +0700

    Up src

---
+++ src/main/java/service/UserService.java
@@ -0,0 +1,8 @@
+package service;
+
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+
+public interface UserService extends UserDetailsService {
+    public UserDetails loadUserByUserName(String userName);
+}
